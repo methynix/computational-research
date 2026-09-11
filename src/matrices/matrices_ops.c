41ccf09aaa7f734a6d15851121f9f9eb9671ce2e
methynix <methodhelen@gmail.com>
2026-09-11 21:21:05 +0300

    Added add,minus,scalar ,transpose and initiated dot product function

--- src/matrices/matrices_ops.c
+++ src/matrices/matrices_ops.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 float** create_matrix(const int rows,const int cols){
 float** mtx=malloc(rows*sizeof(float*));
@@ -22,9 +23,59 @@ void free_matrix(float **mtx,const int rows){
 void print_matrix(const int rows,const int cols,float** mtx){
     for(int i=0; i<rows; i++){
         for(int j=0; j<cols; j++){
-            printf(%)
+            printf("%f",mtx[i][j]);
+            printf("\n");
         }
     }
+}
+
+float** add_matrix(float** mtx_a, float** mtx_b,const int rows,const int cols,float** result){
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+ result[i][j]=mtx_a[i][j]+mtx_b[i][j];
+        }
+    }
+    return result;
+}
+
+float** minus_matrix(float** mtx_a, float** mtx_b,const int rows,const int cols,float** result){
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            result[i][j]=mtx_a[i][j]-mtx_b[i][j];
+        }
+    }
+    return result;
+}
+
+float** scalar_multi(const float scalar,float** mtx,float** result,const int rows,const int cols){
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            result[i][j]=scalar*mtx[i][j];
+        }
+    }
+    return result;
+}
+
+float** transpose_matrix(const int rows,const int cols,float** mtx, float** result){
+for(int i=0; i<rows; i++){
+    for(int j=0; j<rows; j++){
+        result[i][j]=mtx[j][i];
+    }
+}
+return result;
+}
+
+float** matrix_mul(const int rows_A,const int cols_A,const int rows_B,
+                                                const int cols_B, float** mtx_a, float** mtx_b,float** result){
+if(cols_A!=rows_B){
+    printf("ERROR %d Multiplication is impossible because the number of columns of A is not equal to the number of rows of B",errno);
+    return -1
+}
+for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+                                                                      
+    }
+}
 }
 
 int main(){
