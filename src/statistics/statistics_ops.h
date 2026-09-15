f9534153610183a5dcb295c8be044ce044b0ef4c
neema <methodhelen@gmail.com>
2026-09-15 20:15:39 +0300

    Added headers for statistics

--- src/statistics/statistics_ops.h
+++ src/statistics/statistics_ops.h
@@ -1,5 +1,11 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
-float covariance(float* list_a,int ttl_a, float* list_b,int ttl_b);
 
+float return_min(float* vec,const int dim);
+float return_max(float* vec,const int dim);
+float mean(float * arr,const int ttl);
+float var(float * arr,const int ttl);
+float std(float * arr, const int ttl);
+float covariance(float* list_a,int ttl_a, float* list_b,int ttl_b);
+float correlation(float* list_a,float* list_b,const int ttl);
 #endif
